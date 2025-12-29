d805f9d4a054f07d9d3544fec304d0abe7768546
aashishpanchal <aipanchal51@gmail.com>
2025-12-30 04:22:01 +0530

    add: example.ts file

---
+++ example.ts
@@ -0,0 +1,47 @@
+import http from 'http';
+import {RadixTree} from './dist/index.js';
+
+type Req = http.IncomingMessage;
+type Res = http.ServerResponse<http.IncomingMessage> & {
+  req: http.IncomingMessage;
+};
+type Handler = (req: Req, res: Res) => void;
+
+const router = new RadixTree<Handler>();
+
+router.add('ALL', '/', (req, res) => {
+  res.end('Welcome to Radix Way!');
+});
+
+router.add('GET', '/json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({message: 'Hello World'}));
+});
+
+router.add('POST', '/users/:id', (req, res) => {
+  const result = router.match(req.method!, req.url!);
+  const [, paramMap, params] = result!;
+  const userId = params[paramMap.id];
+
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({userId, name: `User ${userId}`}));
+});
+
+const app = http.createServer((req, res) => {
+  const result = router.match(req.method!, req.url!);
+
+  if (!result) {
+    res.statusCode = 404;
+    res.end('Not Found');
+    return;
+  }
+
+  const [handlers] = result;
+  handlers[0](req, res);
+});
+
+app.listen(3000, () => {
+  console.log('🚀 Server running at http://localhost:3000');
+  console.log('\nRadix-Tree:');
+  router.printTree();
+});
